75cfdbe536c1ab60d546507c1b6d8acac7e7c615
Gerhard Siegesmund <jerri@jerri.de>
2006-04-11 22:21:45 +0200

    first version entered.
    This first version is the version I created at work for my own pleasure.
    
    darcs-hash:20060411202145-fd2d1-a61af9153b3c401f662694f0be0159baf5adbc6e.gz

---
+++ screen-graph.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+. $(dirname $0)/functions.sh
+
+function output_help () {
+  echo "Usage: $0 STY screentitle [graphtitle] [wanted]"
+  echo ""
+  echo "STY Name of the screen the data is to be read from."
+  echo "screentitle Title of the window of the screen where the data is shown."
+  echo "[graphtitle] Title of the graph."
+  echo "[wanted]: What value will this graph reach in the end."
+  echo "In this case screen_graph also outputs ETA (based on linear interpolation)."
+  echo ""
+  echo "Use a line like this to create the data-lines for the graph"
+  echo "echo -n \$(date +%s); echo -n ' '; echo \$DATAPOINT"
+  echo ""
+  exit 0
+}
+
+trap "rm -f $tempfile $tempfile.tmp $tempfile.graph; exit 0" HUP KILL TERM
+
+if [ -z "$1" -o -z "$2" ]; then
+  output_help
+else
+  sty=$1
+  graphdata=$2
+fi
+
+if [ -n "$3" ]; then
+  title=$3
+else
+  title=""
+fi
+
+if [ -n "$4" ]; then
+  total=$4
+  minus="-1"
+else
+  total=""
+  minus=""
+fi
+
+tempfile=$(tempfile screengraph)
+
+# resize only works under X.
+#rows=$(resize | grep 'LINES=' | sed -e 's/.*=\([0-9]*\);.*/\1/')
+rows=$(stty -a | grep 'rows' | sed -e 's/.*rows \([0-9]*\);.*/\1/')
+rows=$(( $rows - 1 $minus ))
+
+#cols=$(resize | grep 'COLUMNS=' | sed -e 's/.*=\([0-9]*\);.*/\1/')
+cols=$(stty -a | grep 'columns' | sed -e 's/.*columns \([0-9]*\);.*/\1/')
+
+screen=/usr/bin/screen
+
+# Daten holen
+$screen -S $sty -p $graphdata -X hardcopy -h $tempfile
+
+# Die relevanten Daten herausholen
+cat $tempfile | egrep '^[0-9]* [0-9.]*$' > $tempfile.tmp
+mv $tempfile.tmp $tempfile
+
+# Die Werte fuer die lineare Berechnung rausholen
+firstline=$(head -1 $tempfile)
+lastline=$(tail -1 $tempfile)
+eta=$(echo $firstline $lastline $total | perl -ne 'split (/ +/);$seconds=($_[2]-$_[0])/($_[3]-$_[1])*($_[4] - $_[3]);if ($seconds < 0) { print "Finished" } else { ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=gmtime($seconds);print sprintf ("%d Tag(e) %02d:%02d:%02d", $yday, $hour, $min, $sec); }')
+lastvalue=$(echo $lastline | cut -f 2 -d ' ')
+
+# Die Datumse korrigieren und Veraenderungsdaten eintragen
+cat $tempfile | perl -ne 'split (/ /); ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime($_[0]); chomp ($_[1]); if (defined ($last)) { $diff=($_[1]-$last)/($_[0]-$tlast)*60; } else { $diff=0; }; $tlast=$_[0]; $last=$_[1]; print "".(1900+$year)."-".($mon+1)."-".$mday."_".$hour.":".$min.":".$sec." ".$_[1]." ".$diff."\n";' > $tempfile.tmp
+mv $tempfile.tmp $tempfile
+
+# Graph erstellen lassen
+gnuplot <<EOF
+set grid
+set nokey
+set title "$title"
+set output "$tempfile.graph"
+set terminal dumb $cols $rows
+set timefmt "%Y-%m-%d_%H:%M:%S"
+set xdata time
+set format x '%H:%M'
+set y2tics
+plot "$tempfile" using 1:2 with lines, "$tempfile" using 1:3 axes x1y2 with impulses
+exit
+EOF
+
+cat $tempfile.graph | sed -e 's/\([0-9]\+\)/[1;37m\1[0m/g' -e 's/\(\*\+\)/[31m\1[0m/g' -e 's/\(\#\+\)/[34m\1[0m/g' -e 's/\([.:]\+\)/[1;30m\1[0m/g'
+
+if [ -n "$total" ]; then
+  echo "Now: "$lastvalue" Wanted: "$total" ETA: "$eta
+fi
+
+rm -f $tempfile $tempfile.tmp $tempfile.graph
